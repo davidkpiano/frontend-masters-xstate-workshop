81b940a73e5a484997f204ccd363b27e45da9f1b
David Khourshid <davidkpiano@gmail.com>
2020-05-13 23:11:19 -0400

    Add lesson 5

---
+++ 05/index.js
@@ -0,0 +1,115 @@
+import { createMachine, assign, interpret } from 'xstate';
+
+const elBox = document.querySelector('#box');
+const elBody = document.body;
+
+const assignPoint = assign({
+  px: (context, event) => event.clientX,
+  py: (context, event) => event.clientY,
+});
+
+const assignPosition = assign({
+  x: (context, event) => {
+    return context.x + context.dx;
+  },
+  y: (context, event) => {
+    return context.y + context.dy;
+  },
+  dx: 0,
+  dy: 0,
+  px: 0,
+  py: 0,
+});
+
+const assignDelta = assign({
+  dx: (context, event) => {
+    return event.clientX - context.px;
+  },
+  dy: (context, event) => {
+    return event.clientY - context.py;
+  },
+});
+
+const showDelta = (context) => {
+  elBox.dataset.delta = `delta: ${context.dx}, ${context.dy}`;
+};
+
+const resetPosition = assign({
+  dx: 0,
+  dy: 0,
+  px: 0,
+  py: 0,
+});
+
+const machine = createMachine({
+  initial: 'idle',
+  context: {
+    x: 0,
+    y: 0,
+    dx: 0,
+    dy: 0,
+    px: 0,
+    py: 0,
+  },
+  states: {
+    idle: {
+      on: {
+        mousedown: {
+          actions: assignPoint,
+          target: 'dragging',
+        },
+      },
+    },
+    dragging: {
+      on: {
+        mousemove: {
+          actions: [assignDelta, showDelta],
+          // no target!
+        },
+        mouseup: {
+          actions: assignPosition,
+          target: 'idle',
+        },
+        'keyup.escape': {
+          target: 'idle',
+          actions: resetPosition,
+        },
+      },
+    },
+  },
+});
+
+const service = interpret(machine);
+
+service.onTransition((state) => {
+  if (state.changed) {
+    console.log(state.context);
+
+    elBox.dataset.state = state.value;
+
+    elBox.style.setProperty('--dx', state.context.dx);
+    elBox.style.setProperty('--dy', state.context.dy);
+    elBox.style.setProperty('--x', state.context.x);
+    elBox.style.setProperty('--y', state.context.y);
+  }
+});
+
+service.start();
+
+elBox.addEventListener('mousedown', (event) => {
+  service.send(event);
+});
+
+elBody.addEventListener('mousemove', (event) => {
+  service.send(event);
+});
+
+elBody.addEventListener('mouseup', (event) => {
+  service.send(event);
+});
+
+elBody.addEventListener('keyup', (e) => {
+  if (e.key === 'Escape') {
+    service.send('keyup.escape');
+  }
+});
