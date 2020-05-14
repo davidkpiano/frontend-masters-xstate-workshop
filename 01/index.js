dab6027356614639710c77ecb88cd5169b64dc1d
Chester Heng <chester.heng@gmail.com>
2020-05-15 07:24:11 +0800

    Exercise 01 - Creating a State Machine - switch

--- 01/index.js
+++ 01/index.js
@@ -1,25 +1,42 @@
 const elBox = document.querySelector('#box');
 
+
 // Pure function that returns the next state,
 // given the current state and sent event
-function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
+const transition = (state, event) => {
+  // FSM: ->inactve <-CLICK-> active
+  switch (state) {
+    case "inactive":
+      switch (event) {
+        case "CLICK":
+          return "active";
+        default:
+          return state;  
+      } 
+    case "active":
+      switch (event) {
+        case "CLICK":
+          return "inactive";
+        default:
+          return state;  
+      } 
+    default:
+      return state;  
   }
 }
 
 // Keep track of your current state
-let currentState = undefined;
+// start with inactive initial state
+let currentState = "inactive";
 
-function send(event) {
+const send = event => {
   // Determine the next value of `currentState`
-
+  currentState = transition(currentState, event)
+  console.log(currentState)
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
   // send a click event
+  send("CLICK")
 });
